bf134cebd089f986b51bcf92744a513303e24872
hasanruhan1998 <158472782+hasanruhan1998@users.noreply.github.com>
2024-04-24 18:55:17 +0600

    24.04.24
    math,select

---
+++ MathString/src/mathstring/MathString.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mathstring;
+
+/**
+ *
+ * @author Admin
+ */
+public class MathString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println(Math.abs(-2.50));
+        System.out.println(Math.random());
+        System.out.println(Math.ceil(5.54));
+        System.out.println(Math.pow(5,54));
+        System.out.println(Math.max(5,54));
+        System.out.println(Math.min(5,54));
+    }
+    
+}
